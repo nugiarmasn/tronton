973306fa24cfeb17dda40356ec826362d97b6fe5
nugiarmasnurrizky <nugiarmasnurizky@gmail.com>
2024-01-15 10:37:13 +0700

    terserah

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+public class Main  {
+    public static void main(String[] args) throws IOException {
+        int[] angkasaya = new int[4];
+        angkasaya[0] = 4;
+        angkasaya[1] = 5;
+        angkasaya[2] = 9;
+        angkasaya[3] = 14;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println(angkasaya[1]);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Masukan jumlah data yang diinput : ");
+        int jum_data = Integer.parseInt(reader.readLine());
+        String[] nama = new String[jum_data];
+        for(int indexsaya=0;indexsaya < nama.length;indexsaya++){
+            System.out.println("Masukan nama yang ke - " + (indexsaya+1) + " = ");
+            String nama_input = reader.readLine();
+            nama[indexsaya] = nama_input;
         }
-    }
+        for (int input = 0; input < nama.length;input++){
+            System.out.println("Data nama yang ke "+(input+1)+":");
+            System.out.println("Nama :" +nama[input]);
+   }
+
+
+}
 }
